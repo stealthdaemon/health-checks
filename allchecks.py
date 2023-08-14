bdf91cb412ba1e3cf27a5ee3a0969370eaf10d80
Roby <stealthingdemon@gmail.com>
2023-08-14 08:27:59 +0100

    System Health checks python script
    Create a new script allchecks.py, included all empty functions.
    These functions will be defined accordingly, we run major checks to
    see whether or not a reboot is scheduled and also check disk usage
    and lots more.

---
+++ allchecks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+def check_reboot():
+    pass
+
+
+def check_disk_full():
+    pass
+
+
+def check_no_network():
+    pass
+
+
+def check_cpu_constrained():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
