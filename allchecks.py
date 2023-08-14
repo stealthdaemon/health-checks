d34530ccb6785962fc8ee7b2169a65fa217f5781
Roby <stealthingdemon@gmail.com>
2023-08-14 09:51:38 +0100

    Define check_disk_full function
    use shutil module to check disk usage using the disk_usage function
    Do all necessary conversions to percent and gigabyte also. Then use
    conditional to compare value and return the appropriate boolean value.

--- allchecks.py
+++ allchecks.py
@@ -2,14 +2,24 @@
 
 import os
 import sys
+import shutil
+
 
 def check_reboot():
     """Return True if reboot is scheduled, False if none"""
     return os.path.exists("run/reboot-required")
 
 
-def check_disk_full():
-    pass
+def check_disk_full(disk, min_absolute, min_percent):
+    """Returns True if there isn't enough disk space"""
+    disk_usage = shutil.disk_usage(disk)
+    # calculate free disk percent
+    percent_free = (100 * disk_usage.free) / disk_usage.total
+    # convert free disk space to
+    gigabyte_free = disk_usage.free / 2**30  # 1 billion bytes make up a gb, binary notation is 2**30
+    if gigabyte_free < min_absolute or percent_free < min_percent:
+        return True
+    return False
 
 
 def check_no_network():
@@ -27,6 +37,7 @@ def main():
 
     print("Everything OK!")
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
